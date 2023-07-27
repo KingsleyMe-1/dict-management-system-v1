6ffa9b7149bd11fbacd275956ce753a747f16738
Kingsley James J. Digman <kingsleyjamesjuandigman@gmail.com>
2023-07-27 21:35:36 +0800

    Updated the Add and Edit Modal

--- Computer/computer.js
+++ Computer/computer.js
@@ -1,22 +1,65 @@
-const addBtn = document.querySelector("#add-button");
-const saveBtn = document.querySelector("#save-button");
+const addModal = document.querySelector("#add-modal");
+const viewModal = document.querySelector("#view-modal");
 const editModal = document.querySelector("#edit-modal");
-const editBtn = document.querySelector("#edit-button");
-const viewBtn = document.querySelector("#view-button");
-const exitBtn = document.querySelector("#exit-button");
+
+const addBtn = document.querySelector("#add-button");
+const viewBtn = document.querySelectorAll("#view-button");
+const editBtn = document.querySelectorAll("#edit-button");
+
+const exitAddModalBtn = document.querySelector("#exit-add-modal");
+const exitViewModalBtn = document.querySelector("#exit-view-modal");
+const exitEditModalBtn = document.querySelector("#exit-edit-modal");
 
 addBtn.addEventListener("click", () => {
-  editModal.showModal();
+  addModal.showModal();
 });
 
-editBtn.addEventListener("click", () => {
-  editModal.showModal();
+viewBtn.forEach((view) => {
+  view.addEventListener("click", () => {
+    viewModal.showModal();
+  });
 });
 
-viewBtn.addEventListener("click", () => {
-  editModal.showModal();
+editBtn.forEach((edit) => {
+  edit.addEventListener("click", () => {
+    editModal.showModal();
+  });
 });
 
-exitBtn.addEventListener("click", () => {
+exitAddModalBtn.addEventListener("click", () => {
+  addModal.close();
+});
+
+exitViewModalBtn.addEventListener("click", () => {
+  viewModal.close();
+});
+
+exitEditModalBtn.addEventListener("click", () => {
   editModal.close();
+});
+
+const dragArea = document.querySelector("#drag-area");
+const dragText = document.querySelector(".header");
+const browseFile = document.querySelector("#browse-span");
+const removeFile = document.querySelector("#remove-file");
+const secondText = document.querySelector("#second-text");
+
+dragArea.addEventListener("dragover", (event) => {
+  event.preventDefault();
+  dragText.textContent = "RELEASE & DROP";
+  dragArea.classList.add("active");
+  secondText.classList.add("remove-text");
+});
+
+dragArea.addEventListener("dragleave", () => {
+  dragText.textContent = "DRAG & DROP";
+  dragArea.classList.remove("active");
+  secondText.classList.remove("remove-text");
+});
+
+dragArea.addEventListener("drop", (event) => {
+  event.preventDefault();
+  dragArea.style.border = "2px dashed #1638e3";
+  dragText.textContent = "DRAG & DROP";
+  secondText.classList.remove("remove-text");
 });
